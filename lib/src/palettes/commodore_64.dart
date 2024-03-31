5791ae1bd533568bb03f3bb5cb3404a60edc9da5
Andrii Syrokomskyi <signmotion@gmail.com>
2024-03-31 13:19:33 +0200

    Optimized the palette representations.

--- lib/src/palettes/commodore_64.dart
+++ lib/src/palettes/commodore_64.dart
@@ -1,22 +1,24 @@
-part of '../../uni_color_palette.dart';
+import '../../uni_color_palette.dart';
+
+typedef _C = RgbInt8Color;
 
 /// Source: https://en.wikipedia.org/wiki/Commodore_64#Graphics
 /// Helper sheet: https://docs.google.com/spreadsheets/d/1f8wvrgqfGcXFAiAXx-p9CgLXo3__IoEn8-Us-uRyfok/edit#gid=0
-final commodore64Colors = <RgbInt8Color>[
-  RgbInt8Color.rgbIndexName(0x00, 0x00, 0x00, 0, 'black'),
-  RgbInt8Color.rgbIndexName(0xff, 0xff, 0xff, 1, 'white'),
-  RgbInt8Color.rgbIndexName(0x9f, 0x4e, 0x44, 2, 'red'),
-  RgbInt8Color.rgbIndexName(0x6a, 0xbf, 0xc6, 3, 'cyan'),
-  RgbInt8Color.rgbIndexName(0xa0, 0x57, 0xa3, 4, 'purple'),
-  RgbInt8Color.rgbIndexName(0x5c, 0xab, 0x5e, 5, 'green'),
-  RgbInt8Color.rgbIndexName(0x50, 0x45, 0x9b, 6, 'blue'),
-  RgbInt8Color.rgbIndexName(0xc9, 0xd4, 0x87, 7, 'yellow'),
-  RgbInt8Color.rgbIndexName(0xa1, 0x68, 0x3c, 8, 'orange'),
-  RgbInt8Color.rgbIndexName(0x6d, 0x54, 0x12, 9, 'brown'),
-  RgbInt8Color.rgbIndexName(0xcb, 0x7e, 0x75, 10, 'light red'),
-  RgbInt8Color.rgbIndexName(0x62, 0x62, 0x62, 11, 'dark-gray'),
-  RgbInt8Color.rgbIndexName(0x89, 0x89, 0x89, 12, 'mid-gray'),
-  RgbInt8Color.rgbIndexName(0x9a, 0xe2, 0x9b, 13, 'light green'),
-  RgbInt8Color.rgbIndexName(0x88, 0x7e, 0xcb, 14, 'light blue'),
-  RgbInt8Color.rgbIndexName(0xad, 0xad, 0xad, 15, 'light-gray'),
+final commodore64Colors = [
+  _C.rgbIndexName(0x00, 0x00, 0x00, 0, 'black'),
+  _C.rgbIndexName(0xff, 0xff, 0xff, 1, 'white'),
+  _C.rgbIndexName(0x9f, 0x4e, 0x44, 2, 'red'),
+  _C.rgbIndexName(0x6a, 0xbf, 0xc6, 3, 'cyan'),
+  _C.rgbIndexName(0xa0, 0x57, 0xa3, 4, 'purple'),
+  _C.rgbIndexName(0x5c, 0xab, 0x5e, 5, 'green'),
+  _C.rgbIndexName(0x50, 0x45, 0x9b, 6, 'blue'),
+  _C.rgbIndexName(0xc9, 0xd4, 0x87, 7, 'yellow'),
+  _C.rgbIndexName(0xa1, 0x68, 0x3c, 8, 'orange'),
+  _C.rgbIndexName(0x6d, 0x54, 0x12, 9, 'brown'),
+  _C.rgbIndexName(0xcb, 0x7e, 0x75, 10, 'light red'),
+  _C.rgbIndexName(0x62, 0x62, 0x62, 11, 'dark-gray'),
+  _C.rgbIndexName(0x89, 0x89, 0x89, 12, 'mid-gray'),
+  _C.rgbIndexName(0x9a, 0xe2, 0x9b, 13, 'light green'),
+  _C.rgbIndexName(0x88, 0x7e, 0xcb, 14, 'light blue'),
+  _C.rgbIndexName(0xad, 0xad, 0xad, 15, 'light-gray'),
 ];
--- lib/uni_color_palette.dart
+++ lib/uni_color_palette.dart
@@ -1,12 +1,10 @@
 library;
 
-import 'package:uni_color_model/uni_color_model.dart';
-
 export 'package:uni_color_model/uni_color_model.dart';
 
-part 'src/palettes/commodore_64.dart';
-part 'src/palettes/pantone_fashion_design.dart';
-part 'src/palettes/pantone_graphic_design.dart';
-part 'src/palettes/pantone_product_design.dart';
-part 'src/palettes/ral.dart';
-part 'src/palettes/zeplin.dart';
+export 'src/palettes/commodore_64.dart';
+export 'src/palettes/pantone_fashion_design.dart';
+export 'src/palettes/pantone_graphic_design.dart';
+export 'src/palettes/pantone_product_design.dart';
+export 'src/palettes/ral.dart';
+export 'src/palettes/zeplin.dart';
